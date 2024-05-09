af16f687c00e231713fb2611b5a1150de9ddab42
Issahar Weiss <issahar.wss@gmail.com>
2024-05-09 23:43:30 +0300

    Remove the data

---
+++ ChessStats/ChessStats/Manager/PersistenceManager.swift
@@ -0,0 +1,12 @@
+//
+//  PersistantManager.swift
+//  ChessStats
+//
+//  Created by Issahar Weiss on 09/05/2024.
+//
+
+import Foundation
+
+struct PersistantManager {
+    static let shared = PersistantManager()
+}
---
+++ ChessStats/ChessStats/Model/MonthEndRating.swift
@@ -0,0 +1,8 @@
+//
+//  MonthEndRating.swift
+//  ChessStats
+//
+//  Created by Issahar Weiss on 09/05/2024.
+//
+
+import Foundation
