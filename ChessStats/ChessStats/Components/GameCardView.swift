eb4d92aff515b8db43443e675aa85b8f9f016d2e
Issahar Weiss <issahar.wss@gmail.com>
2024-05-13 10:53:05 +0300

    Added the games view and progress bar on months view

---
+++ ChessStats/ChessStats/Components/GameCardView.swift
@@ -0,0 +1,18 @@
+//
+//  GameCardView.swift
+//  ChessStats
+//
+//  Created by Issahar Weiss on 13/05/2024.
+//
+
+import SwiftUI
+
+struct GameCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameCardView()
+}
