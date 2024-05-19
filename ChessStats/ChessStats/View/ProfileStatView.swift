8c703bd65830b74b8adf6a282c77879abf3be020
Issahar Weiss <issahar.wss@gmail.com>
2024-05-19 14:21:04 +0300

    Show the highest rating in the app

--- ChessStats/ChessStats/View/ProfileStatView.swift
+++ ChessStats/ChessStats/View/ProfileStatView.swift
@@ -17,10 +17,10 @@ struct ProfileStatView: View {
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 15) {
-                GameStatCard(title: "bullet", rating: chessData.profileStat?.bullet.last.rating ?? 0)
-                GameStatCard(title: "blitz", rating: chessData.profileStat?.blitz.last.rating ?? 0)
-                GameStatCard(title: "rapid", rating: chessData.profileStat?.rapid.last.rating ?? 0)
-                GameStatCard(title: "daily", rating: chessData.profileStat?.daily.last.rating ?? 0)
+                GameStatCard(title: "bullet", rating: chessData.profileStat?.bullet.last.rating ?? 0, highest: chessData.profileStat?.bullet.best.rating ?? 0)
+                GameStatCard(title: "blitz", rating: chessData.profileStat?.blitz.last.rating ?? 0, highest: chessData.profileStat?.blitz.best.rating ?? 0)
+                GameStatCard(title: "rapid", rating: chessData.profileStat?.rapid.last.rating ?? 0, highest: chessData.profileStat?.rapid.best.rating ?? 0)
+                GameStatCard(title: "daily", rating: chessData.profileStat?.daily.last.rating ?? 0, highest: chessData.profileStat?.daily.best.rating ?? 0)
             }
             .padding()
         }
@@ -30,27 +30,30 @@ struct ProfileStatView: View {
 struct GameStatCard: View {
     var title: String
     var rating: Int
+    var highest: Int
 
     var body: some View {
         VStack {
+            Text(String(rating))
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding(.top)
+
             Image(title)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .foregroundColor(.yellow)
-                .frame(width: 50, height: 50)
-                .padding(.top)
+                .frame(width: 40, height: 40)
 
-            Text(String(rating))
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.white)
 
-            Text(title.capitalized)
-                .font(.title3)
-                .fontWeight(.bold)
-                .foregroundColor(.white)
-                .padding(.bottom)
-
+            HStack {
+                Image(systemName: "star.fill")
+                Text(String(highest))
+                    .font(.title3)
+                    .foregroundColor(.white)
+            }
+            .padding()
         }
         .frame(width: 115, height: 140)
         .background(Color.gray.opacity(0.5))
